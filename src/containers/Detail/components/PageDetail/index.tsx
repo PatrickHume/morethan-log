3a43af25b4108d6aa27d5c4988e85d94b504069f
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 05:19:36 +0100

    install prismjs from types

--- src/containers/Detail/components/PageDetail/index.tsx
+++ src/containers/Detail/components/PageDetail/index.tsx
@@ -1,46 +1,47 @@
 import { NotionRenderer } from "react-notion-x"
 import { TPost } from "@/src/types"
-import React, { useState, ChangeEvent, useRef, useEffect } from "react"
+import React from "react"
 import dynamic from "next/dynamic"
 import Link from "next/link"
 import Image from "next/image"
 
-import './prism.css'
-import Prism from "prismjs";
-
-export function CodeBlockTile() {
-  const language = "javascript"
-
-  const codeElement = useRef<HTMLElement | null>(null);
-  const [internalText, setInternalText] = useState<string>("")
-
-  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    setInternalText(event.target.value);
-  };
-
-  useEffect(() => {
-    if (codeElement.current) {
-      Prism.highlightElement(codeElement.current)
-    }
-  }, [internalText])
-
-
-  return (
-    <div>
-      <textarea onChange={handleChange} value={internalText}>
-
-      </textarea>
-      <pre>
-        <code ref={codeElement} className={`language-${language}`}>
-          {internalText}
-        </code>
-      </pre>
-    </div>
-  )
-}
-
 const Code = dynamic(() =>
-  import("react-notion-x/build/third-party/code").then(async (m) => m)
+  import("react-notion-x/build/third-party/code").then(async (m) => m))  // additional prism syntaxes
+    await Promise.all([
+      import('prismjs/components/prism-markup-templating.js'),
+      import('prismjs/components/prism-markup.js'),
+      import('prismjs/components/prism-bash.js'),
+      import('prismjs/components/prism-c.js'),
+      import('prismjs/components/prism-cpp.js'),
+      import('prismjs/components/prism-csharp.js'),
+      import('prismjs/components/prism-docker.js'),
+      import('prismjs/components/prism-java.js'),
+      import('prismjs/components/prism-js-templates.js'),
+      import('prismjs/components/prism-coffeescript.js'),
+      import('prismjs/components/prism-diff.js'),
+      import('prismjs/components/prism-git.js'),
+      import('prismjs/components/prism-go.js'),
+      import('prismjs/components/prism-graphql.js'),
+      import('prismjs/components/prism-handlebars.js'),
+      import('prismjs/components/prism-less.js'),
+      import('prismjs/components/prism-makefile.js'),
+      import('prismjs/components/prism-markdown.js'),
+      import('prismjs/components/prism-objectivec.js'),
+      import('prismjs/components/prism-ocaml.js'),
+      import('prismjs/components/prism-python.js'),
+      import('prismjs/components/prism-reason.js'),
+      import('prismjs/components/prism-rust.js'),
+      import('prismjs/components/prism-sass.js'),
+      import('prismjs/components/prism-scss.js'),
+      import('prismjs/components/prism-solidity.js'),
+      import('prismjs/components/prism-sql.js'),
+      import('prismjs/components/prism-stylus.js'),
+      import('prismjs/components/prism-swift.js'),
+      import('prismjs/components/prism-wasm.js'),
+      import('prismjs/components/prism-yaml.js')
+    ])
+    return m.Code
+  })
 )
 const Collection = dynamic(() =>
   import("react-notion-x/build/third-party/collection").then(
