4acf4b004d6c91a78eef022824ad09b8f0f83900
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 16:43:21 +0100

    change style load

--- src/containers/Detail/components/PageDetail/index.tsx
+++ src/containers/Detail/components/PageDetail/index.tsx
@@ -39,41 +39,38 @@ type Props = {
 }
 
 const PageDetail: React.FC<Props> = ({ blockMap, data }) => {
-  const [filesLoaded, setFilesLoaded] = useState(false);
-  useEffect(() => {
-    Promise.all([
-      import('prismjs/components/prism-markup-templating.js'),
-      import('prismjs/components/prism-markup.js'),
-      import('prismjs/components/prism-bash.js'),
-      import('prismjs/components/prism-c.js'),
-      import('prismjs/components/prism-cpp.js'),
-      import('prismjs/components/prism-csharp.js'),
-      import('prismjs/components/prism-java.js'),
-      import('prismjs/components/prism-js-templates.js'),
-      import('prismjs/components/prism-diff.js'),
-      import('prismjs/components/prism-git.js'),
-      import('prismjs/components/prism-go.js'),
-      import('prismjs/components/prism-graphql.js'),
-      import('prismjs/components/prism-glsl.js'),
-      import('prismjs/components/prism-makefile.js'),
-      import('prismjs/components/prism-markdown.js'),
-      import('prismjs/components/prism-objectivec.js'),
-      import('prismjs/components/prism-ocaml.js'),
-      import('prismjs/components/prism-python.js'),
-      import('prismjs/components/prism-rust.js'),
-      import('prismjs/components/prism-solidity.js'),
-      import('prismjs/components/prism-sql.js'),
-      import('prismjs/components/prism-swift.js'),
-      import('prismjs/components/prism-wasm.js'),
-      import('prismjs/components/prism-yaml.js')
-    ]).then(() => {
-      setFilesLoaded(true);
-    });
-  }, []);
 
-  if (!filesLoaded) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    var loadData = async() => {
+      const [initalData, hoverOnData, hoverOffData] = await Promise.all([
+        import('prismjs/components/prism-markup-templating.js'),
+        import('prismjs/components/prism-markup.js'),
+        import('prismjs/components/prism-bash.js'),
+        import('prismjs/components/prism-c.js'),
+        import('prismjs/components/prism-cpp.js'),
+        import('prismjs/components/prism-csharp.js'),
+        import('prismjs/components/prism-java.js'),
+        import('prismjs/components/prism-js-templates.js'),
+        import('prismjs/components/prism-diff.js'),
+        import('prismjs/components/prism-git.js'),
+        import('prismjs/components/prism-go.js'),
+        import('prismjs/components/prism-graphql.js'),
+        import('prismjs/components/prism-glsl.js'),
+        import('prismjs/components/prism-makefile.js'),
+        import('prismjs/components/prism-markdown.js'),
+        import('prismjs/components/prism-objectivec.js'),
+        import('prismjs/components/prism-ocaml.js'),
+        import('prismjs/components/prism-python.js'),
+        import('prismjs/components/prism-rust.js'),
+        import('prismjs/components/prism-solidity.js'),
+        import('prismjs/components/prism-sql.js'),
+        import('prismjs/components/prism-swift.js'),
+        import('prismjs/components/prism-wasm.js'),
+        import('prismjs/components/prism-yaml.js')
+      ]);
+    }
+    loadData();
+  }, [])
 
   return (
     <div className="m-auto max-w-4xl">
