947e3beefac5e432ef92cb211eb338e84f55b12d
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 17:09:52 +0100

    try to store style on page

--- src/containers/Detail/components/PostDetail/index.tsx
+++ src/containers/Detail/components/PostDetail/index.tsx
@@ -10,32 +10,38 @@ import Image from "next/image"
 import Link from "next/link"
 
 import loadable from '@loadable/component';
-const ConfController = loadable(() => {return Promise.all([
-  import('prismjs/components/prism-markup-templating.js'),
-  import('prismjs/components/prism-markup.js'),
-  import('prismjs/components/prism-bash.js'),
-  import('prismjs/components/prism-c.js'),
-  import('prismjs/components/prism-cpp.js'),
-  import('prismjs/components/prism-csharp.js'),
-  import('prismjs/components/prism-java.js'),
-  import('prismjs/components/prism-js-templates.js'),
-  import('prismjs/components/prism-diff.js'),
-  import('prismjs/components/prism-git.js'),
-  import('prismjs/components/prism-go.js'),
-  import('prismjs/components/prism-graphql.js'),
-  import('prismjs/components/prism-glsl.js'),
-  import('prismjs/components/prism-makefile.js'),
-  import('prismjs/components/prism-markdown.js'),
-  import('prismjs/components/prism-objectivec.js'),
-  import('prismjs/components/prism-ocaml.js'),
-  import('prismjs/components/prism-python.js'),
-  import('prismjs/components/prism-rust.js'),
-  import('prismjs/components/prism-solidity.js'),
-  import('prismjs/components/prism-sql.js'),
-  import('prismjs/components/prism-swift.js'),
-  import('prismjs/components/prism-wasm.js'),
-  import('prismjs/components/prism-yaml.js')
-  ])})
+
+const ConfController = loadable(() => {
+    if (localStorage.getItem('prismLanguages')) {
+      return Promise.all([
+        import('prismjs/components/prism-markup-templating.js'),
+        import('prismjs/components/prism-markup.js'),
+        import('prismjs/components/prism-bash.js'),
+        import('prismjs/components/prism-c.js'),
+        import('prismjs/components/prism-cpp.js'),
+        import('prismjs/components/prism-csharp.js'),
+        import('prismjs/components/prism-java.js'),
+        import('prismjs/components/prism-js-templates.js'),
+        import('prismjs/components/prism-diff.js'),
+        import('prismjs/components/prism-git.js'),
+        import('prismjs/components/prism-go.js'),
+        import('prismjs/components/prism-graphql.js'),
+        import('prismjs/components/prism-glsl.js'),
+        import('prismjs/components/prism-makefile.js'),
+        import('prismjs/components/prism-markdown.js'),
+        import('prismjs/components/prism-objectivec.js'),
+        import('prismjs/components/prism-ocaml.js'),
+        import('prismjs/components/prism-python.js'),
+        import('prismjs/components/prism-rust.js'),
+        import('prismjs/components/prism-solidity.js'),
+        import('prismjs/components/prism-sql.js'),
+        import('prismjs/components/prism-swift.js'),
+        import('prismjs/components/prism-wasm.js'),
+        import('prismjs/components/prism-yaml.js')
+        ]);
+    }
+    return ConfController.preload();
+  });
 
 const Code = dynamic(() =>
   import('react-notion-x/build/third-party/code').then((m) => {
@@ -78,8 +84,12 @@ const PostDetail: React.FC<Props> = ({ blockMap, data }) => {
   const category = (data.category && data.category?.[0]) || undefined
 
   useEffect(() => {
-    const data = ConfController.preload();
-}, []);
+    if (!localStorage.getItem('prismLanguages')) {
+      ConfController.preload().then(() => {
+        localStorage.setItem('prismLanguages', 'loaded');
+      });
+    }
+  }, []);
 
   return (
     <div
