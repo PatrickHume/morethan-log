a2537cfd6b9e60c11a7c85954d9ad4b7249bdd79
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 01:17:02 +0100

    update utterances

--- site.config.js
+++ site.config.js
@@ -54,7 +54,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "PatrickHume/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
