ad33a90755affd03469ccc2defcc3553af417e76
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 01:14:27 +0100

    fix card

--- src/containers/Feed/components/cards/MobileProfileCard.tsx
+++ src/containers/Feed/components/cards/MobileProfileCard.tsx
@@ -13,12 +13,11 @@ const MobileProfileCard: React.FC<Props> = () => {
       <div className="p-2 rounded-2xl bg-white dark:bg-zinc-700 mb-4">
         <div className="flex items-center gap-2">
           <Image
-            className="rounded-full"
+            className="relative rounded-full"
             src={CONFIG.profile.image}
             width={90}
             height={90}
             alt="profile_image"
-            className="relative"
           />
           <div className="h-fit dark:text-white">
             <div className=" text-xl  italic font-bold">
