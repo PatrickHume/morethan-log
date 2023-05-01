9955cc464f629e3f94393c164b07eb44708cd1ad
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 16:10:28 +0100

    remove useeffect

--- src/containers/Detail/components/PostDetail/index.tsx
+++ src/containers/Detail/components/PostDetail/index.tsx
@@ -1,47 +1,16 @@
 import { NotionRenderer } from "react-notion-x"
 import dynamic from "next/dynamic"
 import { TPost } from "@/src/types"
-import React, { useEffect } from "react"
+import React from "react"
 import PostHeader from "./PostHeader"
 import Footer from "./PostFooter"
 import CommentBox from "./CommentBox"
 import Category from "@components/Category"
 import Image from "next/image"
 import Link from "next/link"
-import loadable from '@loadable/component';
-
-const ConfController = loadable(() => {return Promise.all([
-  import('prismjs/components/prism-markup-templating.js'),
-  import('prismjs/components/prism-markup.js'),
-  import('prismjs/components/prism-bash.js'),
-  import('prismjs/components/prism-c.js'),
-  import('prismjs/components/prism-cpp.js'),
-  import('prismjs/components/prism-csharp.js'),
-  import('prismjs/components/prism-java.js'),
-  import('prismjs/components/prism-js-templates.js'),
-  import('prismjs/components/prism-diff.js'),
-  import('prismjs/components/prism-git.js'),
-  import('prismjs/components/prism-go.js'),
-  import('prismjs/components/prism-graphql.js'),
-  import('prismjs/components/prism-glsl.js'),
-  import('prismjs/components/prism-makefile.js'),
-  import('prismjs/components/prism-markdown.js'),
-  import('prismjs/components/prism-objectivec.js'),
-  import('prismjs/components/prism-ocaml.js'),
-  import('prismjs/components/prism-python.js'),
-  import('prismjs/components/prism-rust.js'),
-  import('prismjs/components/prism-solidity.js'),
-  import('prismjs/components/prism-sql.js'),
-  import('prismjs/components/prism-swift.js'),
-  import('prismjs/components/prism-wasm.js'),
-  import('prismjs/components/prism-yaml.js')
-  ])})
 
 const Code = dynamic(() =>
-  import('react-notion-x/build/third-party/code').then((m) => {
-    
-    return m.Code;
-  })
+  import('react-notion-x/build/third-party/code').then((m) => m.Code)
 )
 
 const Collection = dynamic(() =>
@@ -76,11 +45,7 @@ type Props = {
 
 const PostDetail: React.FC<Props> = ({ blockMap, data }) => {
   const category = (data.category && data.category?.[0]) || undefined
-
-  useEffect(() => {
-    ConfController.preload();
-}, []);
-
+  
   return (
     <div
       className={`m-auto max-w-4xl bg-white dark:bg-zinc-700 rounded-3xl py-12 px-6 shadow-md`}
