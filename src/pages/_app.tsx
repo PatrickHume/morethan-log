f25b415938c30056933a4fe1167cb0a6cd3241fa
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 03:27:59 +0100

    remove prism-tomorrow

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import 'prismjs/themes/prism.css'
 import 'react-notion-x/src/styles.css'
-import "prismjs/themes/prism-tomorrow.css"; // only needed if you use Code Blocks
 import 'katex/dist/katex.min.css'
 import '@styles/globals.css'
 import '@styles/notion.css'
