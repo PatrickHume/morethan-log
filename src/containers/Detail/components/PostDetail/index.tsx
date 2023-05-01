ed17fc84c01e9ed594a57478e3b670ed824d6eb5
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 17:13:39 +0100

    fix preload

--- src/containers/Detail/components/PostDetail/index.tsx
+++ src/containers/Detail/components/PostDetail/index.tsx
@@ -13,34 +13,34 @@ import loadable from '@loadable/component';
 
 const ConfController = loadable(() => {
     if (localStorage.getItem('prismLanguages')) {
-      return Promise.all([
-        import('prismjs/components/prism-markup-templating.js'),
-        import('prismjs/components/prism-markup.js'),
-        import('prismjs/components/prism-bash.js'),
-        import('prismjs/components/prism-c.js'),
-        import('prismjs/components/prism-cpp.js'),
-        import('prismjs/components/prism-csharp.js'),
-        import('prismjs/components/prism-java.js'),
-        import('prismjs/components/prism-js-templates.js'),
-        import('prismjs/components/prism-diff.js'),
-        import('prismjs/components/prism-git.js'),
-        import('prismjs/components/prism-go.js'),
-        import('prismjs/components/prism-graphql.js'),
-        import('prismjs/components/prism-glsl.js'),
-        import('prismjs/components/prism-makefile.js'),
-        import('prismjs/components/prism-markdown.js'),
-        import('prismjs/components/prism-objectivec.js'),
-        import('prismjs/components/prism-ocaml.js'),
-        import('prismjs/components/prism-python.js'),
-        import('prismjs/components/prism-rust.js'),
-        import('prismjs/components/prism-solidity.js'),
-        import('prismjs/components/prism-sql.js'),
-        import('prismjs/components/prism-swift.js'),
-        import('prismjs/components/prism-wasm.js'),
-        import('prismjs/components/prism-yaml.js')
-        ]);
+      return Promise.resolve();
     }
-    return ConfController.preload();
+    return Promise.all([
+      import('prismjs/components/prism-markup-templating.js'),
+      import('prismjs/components/prism-markup.js'),
+      import('prismjs/components/prism-bash.js'),
+      import('prismjs/components/prism-c.js'),
+      import('prismjs/components/prism-cpp.js'),
+      import('prismjs/components/prism-csharp.js'),
+      import('prismjs/components/prism-java.js'),
+      import('prismjs/components/prism-js-templates.js'),
+      import('prismjs/components/prism-diff.js'),
+      import('prismjs/components/prism-git.js'),
+      import('prismjs/components/prism-go.js'),
+      import('prismjs/components/prism-graphql.js'),
+      import('prismjs/components/prism-glsl.js'),
+      import('prismjs/components/prism-makefile.js'),
+      import('prismjs/components/prism-markdown.js'),
+      import('prismjs/components/prism-objectivec.js'),
+      import('prismjs/components/prism-ocaml.js'),
+      import('prismjs/components/prism-python.js'),
+      import('prismjs/components/prism-rust.js'),
+      import('prismjs/components/prism-solidity.js'),
+      import('prismjs/components/prism-sql.js'),
+      import('prismjs/components/prism-swift.js'),
+      import('prismjs/components/prism-wasm.js'),
+      import('prismjs/components/prism-yaml.js')
+      ]);
   });
 
 const Code = dynamic(() =>
