4e6e8962eff0f2b5174d9e65d5d82cbde705c339
PatrickHume <patrick.hume31@gmail.com>
2023-05-01 09:12:38 +0100

    lazy-load page detail

--- src/containers/Detail/components/PageDetail/index.tsx
+++ src/containers/Detail/components/PageDetail/index.tsx
@@ -1,9 +1,37 @@
 import { NotionRenderer } from "react-notion-x"
 import { TPost } from "@/src/types"
-import React from "react"
+import React, {useEffect} from "react"
 import dynamic from "next/dynamic"
 import Link from "next/link"
 import Image from "next/image"
+import loadable from '@loadable/component';
+
+const ConfController = loadable(() => {return Promise.all([
+  import('prismjs/components/prism-markup-templating.js'),
+  import('prismjs/components/prism-markup.js'),
+  import('prismjs/components/prism-bash.js'),
+  import('prismjs/components/prism-c.js'),
+  import('prismjs/components/prism-cpp.js'),
+  import('prismjs/components/prism-csharp.js'),
+  import('prismjs/components/prism-java.js'),
+  import('prismjs/components/prism-js-templates.js'),
+  import('prismjs/components/prism-diff.js'),
+  import('prismjs/components/prism-git.js'),
+  import('prismjs/components/prism-go.js'),
+  import('prismjs/components/prism-graphql.js'),
+  import('prismjs/components/prism-glsl.js'),
+  import('prismjs/components/prism-makefile.js'),
+  import('prismjs/components/prism-markdown.js'),
+  import('prismjs/components/prism-objectivec.js'),
+  import('prismjs/components/prism-ocaml.js'),
+  import('prismjs/components/prism-python.js'),
+  import('prismjs/components/prism-rust.js'),
+  import('prismjs/components/prism-solidity.js'),
+  import('prismjs/components/prism-sql.js'),
+  import('prismjs/components/prism-swift.js'),
+  import('prismjs/components/prism-wasm.js'),
+  import('prismjs/components/prism-yaml.js')
+  ])})
 
 const Code = dynamic(() =>
   import('react-notion-x/build/third-party/code').then((m) => m.Code)
@@ -39,6 +67,12 @@ type Props = {
 }
 
 const PageDetail: React.FC<Props> = ({ blockMap, data }) => {
+
+
+  useEffect(() => {
+    ConfController.preload();
+}, []);
+
   return (
     <div className="m-auto max-w-4xl">
       <NotionRenderer
